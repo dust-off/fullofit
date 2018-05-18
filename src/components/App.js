f10bf10230fcd624c2484ad1b79fd1384a5d882c
Dustin <dustinburns@gmail.com>
2018-05-18 14:00:43 -0700

    browser popstate implimented

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,30 @@ import Header from './Header';
 import ContestList from './ContestList';
 import Contest from './Contest';
 import * as api from '../api';
+import PropTypes from 'prop-types';
 
 const pushState = (obj, url) =>
   window.history.pushState(obj, '', url);
 
+const onPopState = handler => window.onpopstate = handler;
+
 class App extends React.Component {
   static propTypes = {
-    initialData: React.PropTypes.object.isRequired
+    initialData: PropTypes.object.isRequired
   };
   state = this.props.initialData;
+
+  componentDidMount() {
+    onPopState((event) => {
+      this.setState({
+        currentContestId: (event.state || {}).currentContestId
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    onPopState(null);
+  }
 
   fetchContestList = (contestId) => {
     pushState(
@@ -37,7 +52,7 @@ class App extends React.Component {
       this.setState({
         currentContestId: contest.id,
         contests: {
-          ...this.state.cotests,
+          ...this.state.contests,
           [contest.id]: contest
         }
       });
